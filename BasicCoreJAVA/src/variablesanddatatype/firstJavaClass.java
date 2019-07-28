f1ca6d512c46bb28086a80826e4fb7ae10ac5f0f
mukta <garjemp@gmail.com>
2019-07-28 10:07:27 +0530

    committing my first JAVA Class

---
+++ BasicCoreJAVA/src/variablesanddatatype/firstJavaClass.java
@@ -0,0 +1,12 @@
+package variablesanddatatype;
+
+public class firstJavaClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("First Selenium Class with Var and DT");
+		// TODO Auto-generated method stub
+
+	}
+
+}
