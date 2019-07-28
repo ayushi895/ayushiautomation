0bc4d7d28325134a9a500b5683616c56a27d8368
Ayushi <mohantyayushi62@gmail.com>
2019-07-28 11:07:13 +0530

    Arithmeticoperator

---
+++ BasicCoreJAVA/src/variablesanddatatype/SecondVariableClaa.java
@@ -0,0 +1,18 @@
+package variablesanddatatype;
+
+public class SecondVariableClaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Arithmetic Operators
+		int a=10;
+		int b=20;
+		// +,-,*,/,%
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
