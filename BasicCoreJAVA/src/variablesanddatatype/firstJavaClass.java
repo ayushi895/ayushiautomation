ab5f355d472d9d54891f54db1bd0c83b0a38eca5
Unknown <unknown@example.com>
2019-08-24 12:19:00 +0530

    made some edit to check

--- BasicCoreJAVA/src/variablesanddatatype/firstJavaClass.java
+++ BasicCoreJAVA/src/variablesanddatatype/firstJavaClass.java
@@ -4,7 +4,7 @@ public class firstJavaClass {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("First Selenium Class with Var and DT");
+		System.out.println("First JAVA Selenium Class with Var and DT");
 		// TODO Auto-generated method stub
 
 	}
